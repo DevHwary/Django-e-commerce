058f6056ee19b36bde050cebd936bcb9454dea5d
hwary <mh.hwary@yahoo.com>
2020-05-03 22:27:57 +0200

    fifth commit

--- test/m.py
+++
@@ -1,9 +0,0 @@
-
-    def manage_basket(request):
-        if not request.basket:
-            return render(request, "basket.html", {"formset": None})
-
-if not request.basket:
-            return render(request, "basket.html", {"formset": None})
-
-
